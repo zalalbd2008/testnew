557b42a3dbec56ca9cc2ccc5f5ef7e514c6bb15f
Abid Hasan kiam <kiamhasan267@gmail.com>
2024-05-21 00:54:07 +0600

    fix error

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -233,6 +233,7 @@ const Layout = ({
     <Fragment>
       <Head>
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <meta name="robots" content="noindex" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
         <title>Any Graphics Today</title>

24349c50bceca26dac74c6a2c5883a4edf793b69
Abid Hasan kiam <kiamhasan267@gmail.com>
2024-05-22 22:14:31 +0600

    test

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -233,7 +233,11 @@ const Layout = ({
     <Fragment>
       <Head>
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-        {/* <meta name="robots" content="noindex" /> */}
+        <meta name="robots" content="index, follow" />
+        <meta
+          name="google-site-verification"
+          content="EGzXF4NKX_smb9Rs_AjDjfc3oWbHLBXjGeY2aloiQNY"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
         <title>Any Graphics Today</title>
